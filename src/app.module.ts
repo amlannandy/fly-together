e616de04f24b0a306f756ff44de7dbaa6306ca6e
amlannandy <amlannandy5@gmail.com>
2023-03-14 22:25:49 +0530

    init mongodb conn

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AuthController } from 'auth/auth.controller';
 import { AuthService } from 'auth/auth.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
